709151531d40c9a0ff7dfc837617a9a89956a6ed
Sidney Pho <phosidney@gmail.com>
2020-09-16 22:42:51 -0500

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Sidney Pho on 9/16/20.
+//  Copyright © 2020 sidney.pho@mavs.uta.edu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
